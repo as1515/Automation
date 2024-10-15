93e228e9d4acad8d03cdea9bdd5602594f12daba
as1515 <asaddat87@gmail.com>
2024-10-15 14:24:48 +0600

    Add files via upload

---
+++ Inventory_Value_Tracking/mail.py
@@ -0,0 +1,49 @@
+from pretty_html_table import build_table
+import pandas as pd
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+def send_mail(subject, bodyText, attachment=[], recipient = [], html_body=None):
+    me = "XXXXXX@gmail.com"
+    you = recipient
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = me
+    msg['To'] = ", ".join(you)
+    text = bodyText
+
+    # If html_body is provided, create an HTML part
+    if html_body:
+        html = '<html><body>'
+        html += '<p>' + text + '</p>'
+        for df, heading in html_body:
+            # Add the dataframe with its heading to the email body
+            html += '<h2>' + heading + '</h2>'
+            html += build_table(df, 'blue_dark')
+        html += '</body></html>'
+        part1 = MIMEText(html, 'html')
+    else:
+        part1 = MIMEText(text, 'plain')
+
+    msg.attach(part1)
+
+    # Attachments
+    if attachment:
+        for file_path in attachment:
+            part2 = MIMEBase('application', "octet-stream")
+            part2.set_payload(open(file_path, "rb").read())
+            encoders.encode_base64(part2)
+            part2.add_header('Content-Disposition', 'attachment', filename=file_path)
+            msg.attach(part2)
+
+    username = 'XXXXXX'
+    password = 'XXXXXX'
+
+    s = smtplib.SMTP('smtp.gmail.com:587')
+    s.starttls()
+    s.login(username, password)
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
---
+++ Inventory_Value_Tracking/query_function.py
@@ -0,0 +1,169 @@
+import pandas as pd
+from config import HMBR_LOCAL_SERVER
+
+
+SERVER = HMBR_LOCAL_SERVER
+
+#------------------------- SALES-DISCOUNT-RETURN-QUERY-PART--------------------------#
+home_product_list = ('0787','2079','1780','0790','1571','1779',
+                     '11723','16550','1280','1483','1484','1481',
+                     '1482','1691','17872','0191','1654','2078',
+                     '0788','17870','01951','01953','2077','2073',
+                     '0792','17873','17874','11721','2074','0192',
+                     '01955','0190','01954','07860','17800','2072',
+                     '1655','16540','0793','0786','17220','01952',
+                     '1479','1278','01956','1281','1005','1569',
+                     '1279','1479','1567','1478')
+# get all sales and discount for home_product and trading item
+def sales_discount(zid, start_date, end_date, home_product=None):
+    query = f"""select TO_CHAR(opdor.xdate, 'YYYY-MM') as month, 
+                        sum(opddt.xdtwotax) as sales_amt, sum(opddt.xdtdisc) as disc_amt
+                        from opdor
+                        inner join opddt 
+                        on opdor.xdornum = opddt.xdornum
+                        join caitem
+                        on opddt.xitem=caitem.xitem
+                        where opdor.zid={zid}
+                        and opddt.zid = {zid}
+                        and caitem.zid={zid}
+                        and opdor.xdate between '{start_date}' and '{end_date}'
+                        and opdor.xstatusdor = '3-Invoiced'"""
+
+    if home_product:
+        query += f" and caitem.xitem in {home_product_list}"
+    query += " group by month order by month"
+
+    df = pd.read_sql(query, con=SERVER)
+    return df
+
+# get return amount of home_product and trading item from opcrn (SR--) table
+def sales_return_opcrn(zid, start_date, end_date, home_product=None):
+    query = f"""select TO_CHAR(opcrn.xdate, 'YYYY-MM') as month,  sum (opcdt.xlineamt) as total  from opcrn
+                join opcdt
+                on opcrn.xcrnnum= opcdt.xcrnnum
+                join caitem
+                on opcdt.xitem=caitem.xitem
+                where opcrn.zid={zid}
+                and opcdt.zid = {zid}
+                and caitem.zid ={zid}
+                and opcrn.xdate between '{start_date}' and '{end_date}'
+            """
+
+    if home_product:
+        query += f" and caitem.xitem in {home_product_list}"
+    query += " group by month order by month"
+
+    df = pd.read_sql(query, con=SERVER)
+    return df
+
+# get return amount of home_product and trading item from imtemptrn (RECA, RECT, DSR, etc voucher) table
+def sales_return_imtemptrn(zid, start_date, end_date, home_product=None):
+
+    query = f"""select TO_CHAR(imtemptrn.xdate, 'YYYY-MM') as month,  SUM (imtemptdt.xlineamt) as total  FROM imtemptrn
+                    JOIN imtemptdt
+                    ON imtemptrn.ximtmptrn= imtemptdt.ximtmptrn
+                    JOIN caitem
+                    ON imtemptdt.xitem=caitem.xitem
+                    WHERE imtemptrn.zid={zid}
+                    AND imtemptdt.zid = {zid}
+                    AND caitem.zid ={zid}
+                    AND (imtemptrn.ximtmptrn LIKE '%%RECA%%' OR
+                        imtemptrn.ximtmptrn LIKE '%%SRE-%%' OR
+                        imtemptrn.ximtmptrn LIKE '%%RECT-%%' OR
+                        imtemptrn.ximtmptrn LIKE '%%DSR-%%')
+                    AND imtemptrn.xdate BETWEEN '{start_date}' AND '{end_date}'
+            """
+
+    if home_product:
+        query += f" and caitem.xitem in {home_product_list}"
+    query += " GROUP BY month ORDER BY month"
+
+    df = pd.read_sql(query, con=SERVER)
+    return df
+
+#---------------------- CASH-ANALYSIS-QUERY-PART-----------------------------#
+def get_sales_rate(zid,start_date,end_date):
+    query = f"""SELECT opddt.xitem, AVG(opddt.xdtwotax/opddt.xqty), SUM(opddt.xqty) as total_qty
+                        FROM opddt
+                        JOIN opdor
+                        ON opddt.xdornum = opdor.xdornum
+                        WHERE opdor.zid = {zid}
+                        AND opddt.zid = {zid}
+                        AND opdor.xdate >= '{start_date}'
+                        AND opdor.xdate <= '{end_date}'
+                        GROUP BY opddt.xitem"""
+    df = pd.read_sql ( query, con = SERVER )
+    return df
+
+def get_stock_all(zid, start_date, end_date):
+    query = f"""SELECT xitem, 
+                      SUM(xqty*xsign) AS Stock,
+                      EXTRACT(YEAR FROM xdate) AS year,
+                      EXTRACT(MONTH FROM xdate) AS month
+               FROM imtrn
+               WHERE zid = {zid}
+               AND xdate BETWEEN '{start_date}' AND '{end_date}'
+               GROUP BY xitem, year, month order by xitem"""
+
+    df = pd.read_sql(query, con=SERVER)
+    return df
+
+
+def get_caitem(zid):
+    query = f"""SELECT xitem, xdesc, xgitem, xcitem, xpricecat, xduty, xwh, xstdcost, xstdprice
+                        FROM caitem
+                        WHERE zid = {zid}
+                        AND xgitem = 'Hardware'
+                        OR xgitem = 'Furniture Fittings'
+                        OR xgitem = 'Industrial & Household'
+                        OR xgitem = 'Sanitary'
+                        ORDER BY xgitem ASC"""
+
+    df = pd.read_sql ( query, con = SERVER )
+    return df
+
+
+def get_special_price(zid):
+    query = f"""SELECT xpricecat, xqty,xdisc
+                        FROM opspprc
+                        WHERE zid = {zid}"""
+
+    df = pd.read_sql(query,con = SERVER)
+    return df
+
+
+def price(zid,start_date):
+    query = f"""SELECT opddt.xitem, EXTRACT(MONTH FROM opdor.xdate), AVG(opddt.xdtwotax/opddt.xqty), AVG(opddt.xdiscf), SUM(opddt.xqty) as total_qty
+                        FROM opddt
+                        JOIN opdor
+                        ON opddt.xdornum = opdor.xdornum
+                        WHERE opdor.zid = {zid}
+                        AND opddt.zid = {zid}
+                        AND opddt.xitem IN {home_product_list}
+                        AND opdor.xdate >= '{start_date}'
+                        GROUP BY opddt.xitem, EXTRACT(MONTH FROM opdor.xdate)"""
+    
+    df = pd.read_sql ( query, con = SERVER )
+    return df
+
+def purchase_qty(zid,start_date):
+    query = f"""SELECT imtrn.xitem, imtrn.xyear, imtrn.xper ,SUM(imtrn.xqty)
+                        FROM imtrn
+                        WHERE imtrn.zid = {zid}
+                        AND imtrn.xdocnum LIKE 'GRN-%%'
+                        AND imtrn.xitem IN {home_product_list}
+                        AND imtrn.xdate >= '{start_date}'
+                        GROUP BY imtrn.xitem, imtrn.xyear, imtrn.xper"""
+    df = pd.read_sql ( query, con = SERVER )
+    return df
+
+def get_stock_home(zid,end_date):
+    query = f"""SELECT xitem, SUM(xqty*xsign) as Stock
+                        FROM imtrn
+                        WHERE zid = {zid}
+                        AND xdate <= '{end_date}'
+                        AND xitem IN {home_product_list}
+                        GROUP BY xitem"""
+
+    df = pd.read_sql(query,con = SERVER)
+    return df
